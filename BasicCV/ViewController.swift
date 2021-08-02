9e23a214d62d338954d3e026346ee5f837038723
Ethan <keiserea@gmail.com>
2021-08-02 13:51:15 -0400

    Initial Commit

---
+++ BasicCV/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BasicCV
+//
+//  Created by Ethan Keiser on 8/2/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
